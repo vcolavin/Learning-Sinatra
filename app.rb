dfd4c4cd1ff6e05d7d7befeef3f0ccd9eeafee3c
Vincent Colavin & Vincent Colavin <pair+vcolavin.vcolavin@devbootcamp.com>
2014-12-07 18:41:01 -0800

    this looks nice

--- app.rb
+++ app.rb
@@ -1,9 +1,52 @@
 # app.rb
+require 'sinatra/flash'
+require 'sinatra/redirect_with_flash'
 
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
+enable :sessions
+
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: {minimum: 5}
+  validates :body, presence: true
+end
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome to the home page."
+  erb :"posts/index"
+end
+
+helpers do
+  def title
+    if @title
+      "#{@title}"
+    else
+      "Default title"
+    end
+  end
+end
+
+get "/posts/create" do
+  @title = "create post"
+  @post = Post.new
+  erb :"posts/create"
+end
+
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}", :notice => 'Congrats! Love the new post. (This message will disapear in 4 seconds.)'
+  else
+    redirect :"posts/create", :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
+  end
+end
+
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/view"
 end
