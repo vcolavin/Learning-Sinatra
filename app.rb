2257dcf26c07d89a5bd779f1ce7f05559f6ff0df
Vincent Colavin & Vincent Colavin <pair+vcolavin.vcolavin@devbootcamp.com>
2014-12-07 18:49:29 -0800

    adds editing

--- app.rb
+++ app.rb
@@ -1,23 +1,17 @@
 # app.rb
-require 'sinatra/flash'
-require 'sinatra/redirect_with_flash'
 
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require 'sinatra/flash'
+require 'sinatra/redirect_with_flash'
 
 enable :sessions
 
 
 class Post < ActiveRecord::Base
-  validates :title, presence: true, length: {minimum: 5}
+  validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
-end
-
-get "/" do
-  @posts = Post.order("created_at DESC")
-  @title = "Welcome to the home page."
-  erb :"posts/index"
 end
 
 helpers do
@@ -25,28 +19,48 @@ helpers do
     if @title
       "#{@title}"
     else
-      "Default title"
+      "Welcome."
     end
   end
 end
 
+# get ALL posts
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
+end
+
+# create new post
 get "/posts/create" do
-  @title = "create post"
+  @title = "Create post"
   @post = Post.new
   erb :"posts/create"
 end
-
 post "/posts" do
   @post = Post.new(params[:post])
   if @post.save
     redirect "posts/#{@post.id}", :notice => 'Congrats! Love the new post. (This message will disapear in 4 seconds.)'
   else
-    redirect :"posts/create", :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
+    redirect "posts/create", :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
   end
 end
 
+# view post
 get "/posts/:id" do
   @post = Post.find(params[:id])
   @title = @post.title
   erb :"posts/view"
+end
+
+# edit post
+get "/posts/:id/edit" do
+  @post = Post.find(params[:id])
+  @title = "Edit Form"
+  erb :"posts/edit"
+end
+put "/posts/:id" do
+  @post = Post.find(params[:id])
+  @post.update(params[:post])
+  redirect "/posts/#{@post.id}"
 end
